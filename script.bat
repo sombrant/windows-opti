8434edf1e5a0f352c0b395381e71550e2d2a8436
M4lware <128651023+M4lware-01@users.noreply.github.com>
2023-08-07 19:10:10 +0600

    Update script.bat

--- script.bat
+++ script.bat
@@ -2,7 +2,8 @@
 set ^"|find "::">nul 2>&1 && goto :no || goto :yes
 :no
 chcp 65001 > nul
-echo.&echo "Pour pouvoir utiliser l'utilitaire, vous devez être un administrateur&echo exécutant une session de console."
+echo Pour pouvoir utiliser l'utilitaire, vous devez être un administrateur
+echo exécutant une session de console.
 pause
 chcp 850 > nul
 goto :EOF
